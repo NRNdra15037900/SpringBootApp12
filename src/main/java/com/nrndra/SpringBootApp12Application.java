ed4c5545af17f72800ad9b1a927c827c259a8421
Narendra Gangwar <38030745+NAERNDRAga@users.noreply.github.com>
2022-07-21 10:56:30 +0530

    First Time Commit.

---
+++ src/main/java/com/nrndra/SpringBootApp12Application.java
@@ -0,0 +1,21 @@
+package com.nrndra;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp12Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp12Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Welcome");
+		
+	}
+
+}
---
+++ src/test/java/com/nrndra/SpringBootApp12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrndra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApp12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
